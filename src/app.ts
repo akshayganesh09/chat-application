420e03e28c0b9e954f0f97c7c0a8f85ae7bccb37
Akshay Ganesh <iakshayganesh98@gmail.com>
2026-03-15 17:08:14 +0530

    second-commit

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+    res.status(200).send("Application is up...");
+});
+
 app.get("/health", (req, res) => {
     res.status(200).json("Chat server running...");
 });
