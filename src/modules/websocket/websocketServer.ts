bd05e4dc08b2db0670b2728d7f8b1111269492c6
Akshay Ganesh <iakshayganesh98@gmail.com>
2026-03-18 20:35:34 +0530

    message-progress

--- src/modules/websocket/websocketServer.ts
+++ src/modules/websocket/websocketServer.ts
@@ -2,12 +2,14 @@ import http from "http";
 
 import { Server } from "ws";
 import { verifySocketToken } from "./websocketAuth";
-import { addConnection, removeConnection, sendToUser } from "./connectionManager";
+import { addConnection, getAllConnectedUser, removeConnection, sendToUser } from "./connectionManager";
 import { sendMessage } from "../../services/message.service";
+import { updateLastSeen } from "../../repositories/user.repository";
 
 export const initWebSocketServer = (server: http.Server) => {
   const wss = new Server({ server });
 
+  // Socket Connection...
   wss.on("connection", (socket, request) => {
     const url = new URL(request.url!, `http://${request.headers.host}`);
 
@@ -28,14 +30,32 @@ export const initWebSocketServer = (server: http.Server) => {
     }
 
     addConnection(userId, socket);
-
     console.log(`User connected: ${userId}`);
 
+    // When User-A connects - sever broadcast to everyone User-A is online.
+    broadCast({ type: "USER_ONLINE", payload: { userId } });
+
+    // Socket Message...
     socket.on("message", async (data) => {
       try {
         const message = JSON.parse(data.toString());
         console.log("Message: ", message);
 
+        // User typing...
+        if (message.type === "TYPING_START") {
+          const { receiverId } = message.payload;
+
+          sendToUser(receiverId, { type: "USER_TYPING" });
+        }
+
+        // User typing...
+        if (message.type === "TYPING_STOP") {
+           const { receiverId } = message.payload;
+
+          sendToUser(receiverId, { type: "USER_STOP_TYPING" });
+        }
+
+        // User sends message...
         if (message.type === "SEND_MESSAGE") {
           const { conversationId, receiverId, content } = message.payload;
 
@@ -48,10 +68,24 @@ export const initWebSocketServer = (server: http.Server) => {
       }
     });
 
-    socket.on("close", () => {
+    // Socket Close...
+    socket.on("close", async () => {
       removeConnection(userId);
+
+      await updateLastSeen(userId);
+
+      // When User-A disconnects - sever broadcast to everyone User-A is offline.
+      broadCast({ type: "USER_ONLINE", payload: { userId } });
 
       console.log(`User disconnected: ${userId}`);
     });
   });
+};
+
+export const broadCast = (data: any) => {
+  const users = getAllConnectedUser();
+
+  users.forEach((userId) => {
+    sendToUser(userId, data);
+  })
 };
